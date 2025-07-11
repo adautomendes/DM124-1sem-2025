bfba7db6a1eb0b852351a10ea2d401a28dbc24d6
Adauto Mendes <adauto.inatel@gmail.com>
2025-07-10 21:07:41 -0300

    Adicionando petstore

---
+++ async.js
@@ -0,0 +1,5 @@
+const print = valor => console.log(valor);
+
+setTimeout(() => print("1"), 1000);
+setTimeout(() => print("2"), 1500);
+setTimeout(() => print("3"), 1000);
---
+++ func.js
@@ -0,0 +1,15 @@
+function f1() {
+    console.log("Function 1");
+}
+
+const f2 = function() {
+    console.log("Function 2");
+}
+
+const f3 = () => {
+    console.log("Function 3");
+}
+
+f2();
+f3();
+f1();
