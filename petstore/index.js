7f9d7de462b680360e202edc09213c20ba4a0c9b
Adauto Mendes <adauto.inatel@gmail.com>
2025-07-12 09:37:49 -0300

    Aula 2

--- petstore/index.js
+++ petstore/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
+const database = require('./src/database/config');
 
 const app = express();
+
+app.use(express.json());
 
 app.use(routes);
 
 const porta = 3000;
+
+mongoose.connect(database.DB_URL, database.DB_SETTINGS)
+    .then(() => console.log('Conectado ao MongoDB.'))
+    .catch(erro => console.log(`Erro ao conectar ao MongoDB: ${erro}`));
 
 app.listen(porta, function() {
     console.log(`Petstore rodando na porta ${porta}`);
--- petstore/src/controllers/PetController.js
+++ petstore/src/controllers/PetController.js
@@ -1,8 +1,50 @@
+const Pet = require('../models/Pet');
+
 module.exports = {
-    inserir(req, res) {
-        res.json({ op: 'inserir' })
+    /**
+    POST http://localhost:3000/pet
+    {
+        "nome": "Rex",
+        "raca": "Boxer",
+        "idade": 22
+    }
+    */
+    async inserir(req, res) {
+        const { nome, raca, idade } = req.body;
+
+        const petExistente = await Pet.findOne({ nome });
+
+        if (petExistente) {
+            return res.status(200).json(petExistente);
+        }
+
+        const petCriado = await Pet.create({
+            nome: nome,
+            raca: raca,
+            idade, idade
+        });
+
+        return res.status(201).json(petCriado);
     },
-    buscar(req, res) {
-        res.json({ op: 'buscar' })
+
+    /**
+    GET http://localhost:3000/pet
+    */
+    async buscar(req, res) {
+        const { nome, raca } = req.query;
+
+        // Spread operator
+        let petList = [];
+        if (nome && raca) {
+            petList = await Pet.find({ nome, raca });
+        } else if (nome) {
+            petList = await Pet.find({ nome });
+        } else if (raca) {
+            petList = await Pet.find({ raca });
+        } else {
+            petList = await Pet.find();
+        }
+
+        return res.status(200).json(petList);
     }
 }
